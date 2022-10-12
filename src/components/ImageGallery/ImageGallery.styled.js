1968b94be7f42629226ebb0f906fe88c9277691b
Savchenko-Andrey <dracxm95@gmail.com>
2022-10-12 13:58:55 +0300

    Update ImageGallery.styled.js

--- src/components/ImageGallery/ImageGallery.styled.js
+++ src/components/ImageGallery/ImageGallery.styled.js
@@ -8,7 +8,7 @@ export const Box = styled.div`
 
 export const ImgGalleryList = styled.ul`
   display: grid;
-  grid-template-columns: repeat(3, 350px);
+  grid-template-columns: repeat(3, 400px);
   grid-gap: 10px;
   padding: 10px;
   list-style: none;
